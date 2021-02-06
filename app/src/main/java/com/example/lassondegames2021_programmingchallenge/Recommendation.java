bf143fcdaae5479a2e3293cb04d06b6d77354600
lemyk <leymk@my.yorku.ca>
2021-02-06 13:10:56 -0500

    evaluation

--- app/src/main/java/com/example/lassondegames2021_programmingchallenge/Recommendation.java
+++ app/src/main/java/com/example/lassondegames2021_programmingchallenge/Recommendation.java
@@ -2,13 +2,36 @@ package com.example.lassondegames2021_programmingchallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class Recommendation extends AppCompatActivity {
-
+    int eval = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendation);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            eval = extras.getInt("eval_score");
+        }
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.lRecomm);
+        ll.addView(recommendations(eval));
+
+    }
+
+    public TextView recommendations(int eval){
+        TextView tv = new TextView(this);
+        tv.setTextColor(Color.BLACK);
+        tv.setTextSize(18f);
+        tv.setText("hello");
+
+
+        return tv;
     }
 }
