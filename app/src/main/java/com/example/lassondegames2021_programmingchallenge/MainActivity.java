2f9f6a28328dd121a65736c4c78ca0ba40620a35
lemyk <leymk@my.yorku.ca>
2021-02-05 19:54:58 -0500

    initial

---
+++ app/src/main/java/com/example/lassondegames2021_programmingchallenge/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lassondegames2021_programmingchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
